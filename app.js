5ae18bcaa9169dba16cce84025fd771061e0a638
Saulo Andrioli <saulo.andrioli@gmail.com>
2019-08-10 15:00:21 -0300

    adicionado cors

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
-var logger = require('morgan')
-var express = require('express')
-var parser = require('body-parser')
-var app = express()
+const logger = require('morgan')
+const express = require('express')
+const parser = require('body-parser')
+const cors = require('cors')
 
-/* Routes */
-
-var routeNotas = require('./routes/route_notas')
-var routeFaltas = require('./routes/route_faltas')
-var routeHorarios = require('./routes/route_horarios')
-var routeMaterias = require('./routes/route_materias')
-var routePeriodos = require('./routes/route_periodos')
-var routeAulas = require('./routes/route_aulas')
-var routeUsuarios = require('./routes/route_usuarios')
+const app = express()
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(parser.json())
+app.use(cors())
 
 app.use('/', require('./routes/index_route'))
-app.use('/notas', routeNotas)
-app.use('/faltas', routeFaltas)
-app.use('/horarios', routeHorarios)
-app.use('/materias', routeMaterias)
-app.use('/periodos', routePeriodos)
-app.use('/aulas', routeAulas)
-app.use('/auth', routeUsuarios)
+app.use('/notas', require('./routes/route_notas'))
+app.use('/faltas', require('./routes/route_faltas'))
+app.use('/horarios', require('./routes/route_horarios'))
+app.use('/materias', require('./routes/route_materias'))
+app.use('/periodos', require('./routes/route_periodos'))
+app.use('/aulas', require('./routes/route_aulas'))
+app.use('/auth', require('./routes/route_usuarios'))
 
 app.use((err, req, res, next) => {
   if (err.code === 404) {
