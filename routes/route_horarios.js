77976a5d6b36af535e603f13f732dd041c369a03
Saulo Andrioli <saulo.andrioli@gmail.com>
2019-11-01 00:36:08 -0300

    adicionada rotina para exclusao de usuarios

--- routes/route_horarios.js
+++ routes/route_horarios.js
@@ -1,16 +1,16 @@
 const route = require('express').Router()
-const model = require('../models').horarios
+const models = require('../models')
 const HorariosDTO = require('../dto/horarios_dto')
 const helper = require('../utils/route_helper')
 const authMid = require('../middleware/auth')
 route.use(authMid)
 
 route.post('/', async (req, res, next) => {
-  helper.createAndRespond(res, next, model, HorariosDTO(req.body))
+  helper.createAndRespond(res, next, models.horarios, HorariosDTO(req.body))
 })
 
 route.delete('/:id', async (req, res, next) => {
-  helper.deleteAndRespond(res, next, model, {
+  helper.deleteAndRespond(res, next, models.horarios, {
     where: {
       id: req.params.id
     }
@@ -18,7 +18,7 @@ route.delete('/:id', async (req, res, next) => {
 })
 
 route.get('/:idperiodo', async (req, res, next) => {
-  helper.findAllAndRespond(res, next, model, {
+  helper.findAllAndRespond(res, next, models.horarios, {
     where: {
       periodoId: req.params.idperiodo
     }
--- routes/route_usuarios.js
+++ routes/route_usuarios.js
@@ -3,18 +3,30 @@ const models = require('../models')
 const auth = require('../utils/auth_helper')
 const moveNext = require('../utils/route_helper').moveNext
 
-async function getData () {
+async function getData() {
   const periodos = await models.periodos.findAll({
     include: [
       {
-        model: models.horarios
+        model: models.horarios,
+        hierarchy: true,
+        as: 'horarios'
       },
       {
         model: models.materias,
+        hierarchy: true,
         include: [
-          { model: models.aulas },
-          { model: models.faltas },
-          { model: models.notas }
+          {
+            model: models.aulas,
+            hierarchy: true
+          },
+          {
+            model: models.faltas,
+            hierarchy: true
+          },
+          {
+            model: models.notas,
+            hierarchy: true
+          }
         ]
       }
     ]
@@ -86,5 +98,38 @@ route.post('/login', async (req, res, next) => {
     next(error)
   }
 })
+
+route.delete('/unregister', async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    if (!email) {
+      moveNext(next, 400, "Email não cadastrado")
+    } else if (!password) {
+      moveNext(next, 400, "Senha não fornecida")
+    } else {
+      const usuario = await models.usuarios.findOne({
+        where: {
+          email: email
+        }
+      })
+
+      if (!usuario) {
+        moveNext(next, 400, "Email não encontrado")
+      } else if (!await auth.compareHash(password, usuario.password)) {
+        moveNext(next, 400, "Senha inválida")
+      } else {
+        await models.usuarios.destroy({
+          where: {
+            email: email
+          }
+        })
+
+        res.status(200).send({ "status": "ok" })
+      }
+    }
+  } catch (ex) {
+    moveNext(next, 400, "Falha ao descadastrar")
+  }
+});
 
 module.exports = route
